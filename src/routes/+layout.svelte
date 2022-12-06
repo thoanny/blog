e39e0db13c1d53cd6a2264496ba18b4d249f7ea0
Anthony Destenay <anthony.destenay@gmail.com>
2022-12-06 09:52:42 +0100

    Followed tutorial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/settings">Settings</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+  
+    export let data: LayoutData;
+  </script>
+  
+  <h1>Settings</h1>
+  
+  <div class="submenu">
+    {#each data.sections as section}
+      <a href="/settings/{section.slug}">{section.title}</a>
+    {/each}
+  </div>
+  
+  <slot></slot>
