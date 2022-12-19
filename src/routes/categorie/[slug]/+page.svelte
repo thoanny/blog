8d5165eb600da5845b8755c12e82ddb8e6fe1ac8
Anthony Destenay <anthony.destenay@gmail.com>
2022-12-19 17:02:57 +0100

    Fixed no post in category or tag

--- src/routes/categorie/[slug]/+page.svelte
+++ src/routes/categorie/[slug]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import type { PageData } from './$types';
-  import Posts from '$lib/Posts.svelte';
-  export let data: PageData;
+	import type { PageData } from './$types';
+	import Posts from '$lib/Posts.svelte';
+	export let data: PageData;
 </script>
 
 <svelte:head>
@@ -9,6 +9,26 @@
 </svelte:head>
 
 <div>
-  <h1 class="text-4xl mb-6 font-bold">Catégorie&nbsp;: {data.category}</h1>
-  <Posts {...data} />
+	<h1 class="text-4xl mb-6 font-bold">Catégorie&nbsp;: {data.category}</h1>
+	{#if data.posts.length > 0}
+		<Posts {...data} />
+	{:else}
+		<div class="alert alert-info shadow-lg font-semibold">
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="stroke-current flex-shrink-0 w-6 h-6"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>Aucun article dans cette catégorie.</span>
+			</div>
+		</div>
+	{/if}
 </div>
--- src/routes/mot-cle/[slug]/+page.svelte
+++ src/routes/mot-cle/[slug]/+page.svelte
@@ -10,5 +10,25 @@
 
 <div>
 	<h1 class="text-4xl mb-6 font-bold">Mot-clé&nbsp;: {data.tag}</h1>
-	<Posts {...data} />
+	{#if data.posts.length > 0}
+		<Posts {...data} />
+	{:else}
+		<div class="alert alert-info shadow-lg font-semibold">
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="stroke-current flex-shrink-0 w-6 h-6"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>Aucun article avec ce mot-clé.</span>
+			</div>
+		</div>
+	{/if}
 </div>
