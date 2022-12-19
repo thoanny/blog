8b82323f58385efc308e80e4d91f35e410e4ecbc
Anthony Destenay <anthony.destenay@gmail.com>
2022-12-19 16:58:37 +0100

    Hidded save username and email in comment add form

--- src/lib/AddComment.svelte
+++ src/lib/AddComment.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-    import { createForm } from "svelte-forms-lib";
-    import * as yup from "yup";
-    import { PUBLIC_GRAPHQL_URL } from '$env/static/public';
+	import { createForm } from 'svelte-forms-lib';
+	import * as yup from 'yup';
+	import { PUBLIC_GRAPHQL_URL } from '$env/static/public';
 
-    export let postId;
+	export let postId;
 
-    let commentError = [];
-    let commentSuccess = false;
-    let lockedComment = false;
+	let commentError = [];
+	let commentSuccess = false;
+	let lockedComment = false;
 
-    async function addComment(data, pid) {
+	async function addComment(data, pid) {
+		if (data.subject) {
+			commentSuccess = true;
+			return;
+		}
 
-      if(data.subject) {
-        commentSuccess = true;
-        return;
-      }
-
-      const query = `
+		const query = `
         mutation CREATE_COMMENT($author: String, $authorEmail: String, $commentOn: Int, $content: String) {
           createComment(
             input: {commentOn: $commentOn, content: $content, author: $author, authorEmail: $authorEmail}
@@ -38,148 +37,173 @@
         }
       `;
 
-      commentError = [];
+		commentError = [];
 
-      const response = await fetch(PUBLIC_GRAPHQL_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 
-            query: query,
-            variables: {
-              'author': data.name,
-              'authorEmail': data.email,
-              'commentOn': pid,
-              'content': data.content              
-            }
-        }),
-      });
+		const response = await fetch(PUBLIC_GRAPHQL_URL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				query: query,
+				variables: {
+					author: data.name,
+					authorEmail: data.email,
+					commentOn: pid,
+					content: data.content
+				}
+			})
+		});
 
-      if (response.ok) {
-        const res = await response.json();
-        lockedComment = false;
-        
-        if(!res.data.createComment) {
-          commentError = res.errors;
-          return;
-        }
+		if (response.ok) {
+			const res = await response.json();
+			lockedComment = false;
 
-        commentSuccess = true;
-        return;
-      }
-    }
+			if (!res.data.createComment) {
+				commentError = res.errors;
+				return;
+			}
 
-    const { form, errors, state, handleChange, handleSubmit } = createForm({
-        initialValues: {
-            name: "",
-            email: "",
-            content: "",
-            save: false,
-            privacy: false,
-            subject: "",
-        },
-        validationSchema: yup.object().shape({
-            name: yup.string().required(),
-            email: yup.string().email(),
-            content: yup.string().required(),
-            save: yup.bool(),
-            privacy: yup.bool().required(),
-            subject: yup.string()
-        }),
-            onSubmit: values => {
-              lockedComment = true;
-              addComment(values, postId);
-        }
-    });
+			commentSuccess = true;
+			return;
+		}
+	}
+
+	const { form, errors, state, handleChange, handleSubmit } = createForm({
+		initialValues: {
+			name: '',
+			email: '',
+			content: '',
+			save: false,
+			privacy: false,
+			subject: ''
+		},
+		validationSchema: yup.object().shape({
+			name: yup.string().required(),
+			email: yup.string().email(),
+			content: yup.string().required(),
+			save: yup.bool(),
+			privacy: yup.bool().required(),
+			subject: yup.string()
+		}),
+		onSubmit: (values) => {
+			lockedComment = true;
+			addComment(values, postId);
+		}
+	});
 </script>
 
 <div>
-  {#if commentSuccess}
-    <div class="alert alert-success bg-primary shadow-lg">
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>Commentaire enregistré et en attente de validation.</span>
-      </div>
-    </div>
-  {:else}
-    <form class="shadow-xl rounded-xl p-4 border {(commentSuccess)?'hidden':''}" on:submit={handleSubmit} autocomplete="off">
-      <h2 class="text-2xl mb-2 font-semibold">Ajouter un commentaire</h2>
-      {#if commentError.length > 0}
-        <div class="alert alert-error shadow-lg !mt-4 !mb-2">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <div>
-              {#each commentError as error}
-                <p>{error.message}</p>
-              {/each}
-            </div>
-          </div>
-        </div>
-        
-      {/if}
-      <div class="flex gap-4">
-        <div class="form-control w-full">
-          <label class="label required" for="name">
-            <span class="label-text">Pseudonyme</span>
-          </label>
-          <input 
-              type="text" 
-              placeholder="John Doe" 
-              id="name"
-              name="name"
-              class="input input-bordered w-full" 
-              on:change={handleChange}
-              on:blur={handleChange}
-              bind:value={$form.name} 
-          />
-          {#if $errors.name}
-              <span class="label">
-                  <span class="label-text-alt text-error">{$errors.name}</span>
-              </span>
-          {/if}
-        </div>
-        <div class="form-control w-full">
-          <label class="label" for="email">
-            <span class="label-text">Adresse e-mail</span>
-          </label>
-          <input 
-              type="text" 
-              placeholder="john.doe@fai.ext" 
-              id="email"
-              name="email"
-              class="input input-bordered w-full" 
-              on:change={handleChange}
-              on:blur={handleChange}
-              bind:value={$form.email} 
-          />
-          {#if $errors.email}
-              <span class="label">
-                  <span class="label-text-alt text-error">{$errors.email}</span>
-              </span>
-          {/if}
-        </div>
-      </div>
-      <div class="form-control">
-          <label class="label required" for="content">
-              <span class="label-text">Commentaire</span>
-          </label> 
-          <textarea 
-              class="textarea textarea-bordered h-36"
-              id="content" 
-              name="content" 
-              placeholder=""
-              on:change={handleChange}
-              on:blur={handleChange}
-              bind:value={$form.content} 
-          ></textarea>
-          {#if $errors.content}
-              <span class="label">
-                  <span class="label-text-alt text-error">{$errors.content}</span>
-              </span>
-          {/if}
-      </div>
-      <div class="form-control mt-1">
+	{#if commentSuccess}
+		<div class="alert alert-success bg-primary shadow-lg">
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="stroke-current flex-shrink-0 h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>Commentaire enregistré et en attente de validation.</span>
+			</div>
+		</div>
+	{:else}
+		<form
+			class="shadow-xl rounded-xl p-4 border {commentSuccess ? 'hidden' : ''}"
+			on:submit={handleSubmit}
+			autocomplete="off"
+		>
+			<h2 class="text-2xl mb-2 font-semibold">Ajouter un commentaire</h2>
+			{#if commentError.length > 0}
+				<div class="alert alert-error shadow-lg !mt-4 !mb-2">
+					<div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="stroke-current flex-shrink-0 h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							><path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/></svg
+						>
+						<div>
+							{#each commentError as error}
+								<p>{error.message}</p>
+							{/each}
+						</div>
+					</div>
+				</div>
+			{/if}
+			<div class="flex gap-4">
+				<div class="form-control w-full">
+					<label class="label required" for="name">
+						<span class="label-text">Pseudonyme</span>
+					</label>
+					<input
+						type="text"
+						placeholder="John Doe"
+						id="name"
+						name="name"
+						class="input input-bordered w-full"
+						on:change={handleChange}
+						on:blur={handleChange}
+						bind:value={$form.name}
+					/>
+					{#if $errors.name}
+						<span class="label">
+							<span class="label-text-alt text-error">{$errors.name}</span>
+						</span>
+					{/if}
+				</div>
+				<div class="form-control w-full">
+					<label class="label" for="email">
+						<span class="label-text">Adresse e-mail</span>
+					</label>
+					<input
+						type="text"
+						placeholder="john.doe@fai.ext"
+						id="email"
+						name="email"
+						class="input input-bordered w-full"
+						on:change={handleChange}
+						on:blur={handleChange}
+						bind:value={$form.email}
+					/>
+					{#if $errors.email}
+						<span class="label">
+							<span class="label-text-alt text-error">{$errors.email}</span>
+						</span>
+					{/if}
+				</div>
+			</div>
+			<div class="form-control">
+				<label class="label required" for="content">
+					<span class="label-text">Commentaire</span>
+				</label>
+				<textarea
+					class="textarea textarea-bordered h-36"
+					id="content"
+					name="content"
+					placeholder=""
+					on:change={handleChange}
+					on:blur={handleChange}
+					bind:value={$form.content}
+				/>
+				{#if $errors.content}
+					<span class="label">
+						<span class="label-text-alt text-error">{$errors.content}</span>
+					</span>
+				{/if}
+			</div>
+			<!-- <div class="form-control mt-1">
         <label class="label cursor-pointer justify-start gap-2">
           <input type="checkbox" class="toggle toggle-primary" name="save" on:change={handleChange} on:blur={handleChange} bind:value={$form.save} />
           <span class="label-text">Enregistrer mon pseudonyme et adresse e-mail</span>
@@ -189,42 +213,55 @@
             </svg>              
           </div>
         </label>
-      </div>
-      <div class="form-control">
-        <label class="label cursor-pointer justify-start gap-2">
-          <input type="checkbox" class="toggle toggle-primary" required />
-          <span class="label-text">J'accepte la politique de confidentialité</span>
-        </label>
-      </div>
+      </div> -->
+			<div class="form-control">
+				<label class="label cursor-pointer justify-start gap-2">
+					<input type="checkbox" class="toggle toggle-primary" required />
+					<span class="label-text">J'accepte la politique de confidentialité</span>
+				</label>
+			</div>
 
-      <input type="hidden" name="subject" id="subject" on:change={handleChange} on:blur={handleChange} bind:value={$form.subject}>
-      
-      <button type="submit" disabled={lockedComment} class="btn btn-block mt-2 {(lockedComment)?'loading btn-disabled':'btn-primary'}">
-        {#if lockedComment}Merci de patienter...{:else}Envoyer mon commentaire{/if}
-      </button>
-    </form>
-  {/if}
+			<input
+				type="hidden"
+				name="subject"
+				id="subject"
+				on:change={handleChange}
+				on:blur={handleChange}
+				bind:value={$form.subject}
+			/>
+
+			<button
+				type="submit"
+				disabled={lockedComment}
+				class="btn btn-block mt-2 {lockedComment ? 'loading btn-disabled' : 'btn-primary'}"
+			>
+				{#if lockedComment}Merci de patienter...{:else}Envoyer mon commentaire{/if}
+			</button>
+		</form>
+	{/if}
 </div>
 
 <style lang="scss">
-  form, .alert {
-        @apply max-w-2xl mx-auto my-8;
-  }
+	form,
+	.alert {
+		@apply max-w-2xl mx-auto my-8;
+	}
 
-  form {
-      input, textarea {
-          @apply text-sm;
-      }
+	form {
+		input,
+		textarea {
+			@apply text-sm;
+		}
 
-      .label {
-        &.required {
-          .label-text {
-            &:after {
-              @apply text-error;
-              content: '*';
-            }
-          }
-        }
-      }
-  }
+		.label {
+			&.required {
+				.label-text {
+					&:after {
+						@apply text-error;
+						content: '*';
+					}
+				}
+			}
+		}
+	}
 </style>
