121da26ab191344108658f8ac98a7f277de1e561
Anthony Destenay <anthony.destenay@gmail.com>
2023-01-04 12:29:20 +0100

    Added default SEO image

--- src/lib/SEO.svelte
+++ src/lib/SEO.svelte
@@ -2,17 +2,17 @@
 	import { stripHtml } from 'string-strip-html';
 	import { page } from '$app/stores';
 
+	const url = new URL($page.url);
+	const cleanUrl = `${url.protocol}//${url.host}${url.pathname}`;
+
 	export let title: string;
 	export let description: string;
-	export let image: string = '@TODO';
+	export let image: string = `${url.protocol}//${url.host}/preview.png`;
 
 	title = title ? `${title} | Thoanny` : 'Thoanny';
 	description = description
 		? stripHtml(description).result
 		: 'Blog de Thoanny, où sont partagés des actualités de ses projets personnels, des articles sur le thème du streaming, du jeu vidéo, et aussi des trucs et astuces, des recettes de cuisine...';
-
-	const url = new URL($page.url);
-	const cleanUrl = `${url.protocol}//${url.host}${url.pathname}`;
 </script>
 
 <svelte:head>
